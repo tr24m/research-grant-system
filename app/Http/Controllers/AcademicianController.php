9bb86fe39818abd1a2c481685410fc8aa60f8dc6
mosmn <ithinkisee8@gmail.com>
2025-01-13 10:25:27 +0800

    update

--- app/Http/Controllers/AcademicianController.php
+++ app/Http/Controllers/AcademicianController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class AcademicianController extends Controller
 {
+    #f
     public function index() {
         $academicians = Academician::all();
         return view('academicians.index', compact('academicians'));
